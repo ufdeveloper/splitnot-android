d94164570dbaf59a26d08cf06c6fd281fea6c12f
Shantanu Sardal <shantanu.sardal@logmein.com>
2019-11-01 17:16:17 -0400

    Use volley for network requests

--- app/src/main/java/com/megshan/splitnotandroid/MainActivity.java
+++ app/src/main/java/com/megshan/splitnotandroid/MainActivity.java
@@ -3,14 +3,37 @@ package com.megshan.splitnotandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.megshan.splitnotandroid.utils.RequestQueueUtil;
 import com.plaid.splitnotandroid.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOGGER = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get a request queue
+        RequestQueue requestQueue =
+                RequestQueueUtil.getInstance(this.getApplicationContext()).getRequestQueue();
+
+        // Create request
+        String url = "http://10.0.2.2:8080/items?userKey=123";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (Request.Method.GET, url, null,
+                        (response -> Log.i(LOGGER, response.toString())),
+                        (error -> Log.e(LOGGER, "error fetching data from splitnot-api, error=" + error))
+                );
+
+        // Add request to queue
+        RequestQueueUtil.getInstance(this.getApplicationContext()).addToRequestQueue(jsonArrayRequest);
     }
 }
---
+++ app/src/main/java/com/megshan/splitnotandroid/utils/RequestQueueUtil.java
@@ -0,0 +1,38 @@
+package com.megshan.splitnotandroid.utils;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class RequestQueueUtil {
+    private static RequestQueueUtil instance;
+    private RequestQueue requestQueue;
+    private static Context ctx;
+
+    private RequestQueueUtil(Context context) {
+        ctx = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized RequestQueueUtil getInstance(Context context) {
+        if (instance == null) {
+            instance = new RequestQueueUtil(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            requestQueue = Volley.newRequestQueue(ctx.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+}
